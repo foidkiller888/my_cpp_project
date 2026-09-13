d92dc21f253b644532f8811252572cf37d76d47c
Ilya Eliseev <popgpooa@gmail.com.com>
2026-09-13 19:27:22 +0300

    Update message in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main() { 
-    std::cout << "Hello, my name is Ilya Eliseev. I am from IU5-12/4!" << std::endl; 
+    std::cout << "hi!" << std::endl; 
     return 0; 
 } 
